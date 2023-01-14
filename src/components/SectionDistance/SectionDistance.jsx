07d3cc84e8479bbfe0b1301ea644e537a6c77d9f
2Sodol <104712554+dex7688@users.noreply.github.com>
2023-01-14 17:45:20 +0900

    change port 80 for setting https

--- src/components/SectionDistance/SectionDistance.jsx
+++ src/components/SectionDistance/SectionDistance.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import styles from './SectionDistance.module.css';
 import useGeolocation from '../useGeolocation';
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
 export default function SectionDistance() {
   const [slide, setSlide] = useState(0);
-  const [distanceData, loading] = useGeolocation();
+  const [distanceData, loading, error] = useGeolocation();
+  const navigate = useNavigate();
 
   const getCarouselStyle = () => {
     return {
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import styles from './Main.module.css';
 import SectionCarousel from '../../components/SectionCarousel/SectionCarousel';
-import SectionTourlist from '../../components/SectionTourlist/SectionTourlist';
 import SectionDistance from '../../components/SectionDistance/SectionDistance';
+import SectionTourlist from '../../components/SectionTourlist/SectionTourlist';
 
 export default function Main() {
   return (
